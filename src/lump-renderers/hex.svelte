936cda9654a706caf670f024feccd4573e0b7cc9
alligator <enemy.forest.brigade@gmail.com>
2024-01-01 20:33:26 +0000

    fix hex rendering

--- src/lump-renderers/hex.svelte
+++ src/lump-renderers/hex.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
   export let lump: Uint8Array;
   const rowSize = 16;
+
   let rowIndices: number[];
-  $: rowIndices = new Array(Math.ceil(lump.length / rowSize)).fill(0).map((_, idx) => idx * rowSize);
+  let rows: Uint8Array[];
+  $: {
+    rowIndices = new Array(Math.ceil(lump.length / rowSize)).fill(0).map((_, idx) => idx * rowSize);
+    rows = rowIndices.map((index) => {
+      const row = new Uint8Array(rowSize);
+      for (let i = 0; i < rowSize; i++) {
+        row[i] = lump[index + i] ?? 0;
+      }
+      return row;
+    });
+  }
+
+  function pad(n) {
+    return n.length === 2 ? n : `0${n}`;
+  }
 
   function isPrintable(char: string) {
     return !(/[\x00-\x1F\x80-\xFF]/.test(char));
@@ -18,11 +33,11 @@
         <th>text</th>
     </thead>
     <tbody>
-      {#each rowIndices as index}
+      {#each rows as row, index}
         <tr>
-          <td>{index.toString(16)}</td>
-          {#each lump.slice(index, index + rowSize) as byte}
-            <td>{byte.toString(16)}</td>
+          <td>{(index * rowSize).toString(16)}</td>
+          {#each row as byte}
+            <td class={byte === 0 ? 'dim' : ''}>{pad(byte.toString(16))}</td>
           {/each}
           <td>
             {Array.from(lump.slice(index, index + rowSize)).map(c => {
@@ -39,11 +54,13 @@
 <style>
   table {
     font-family: monospace;
+    border-spacing: .5rem 0;
   }
-  table tr > td:first-child {
-    border-right: 1px solid #555;
+  table td {
+    min-width: 2ex;
   }
-  table tr > td:last-child {
-    border-left: 1px solid #555;
+
+  table td.dim {
+    color: #777;
   }
 </style>
